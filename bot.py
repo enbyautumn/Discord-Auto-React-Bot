4a76035bdc6d146a6fabca6cb1f678a216a18a4e
enbyautumn <60362123+enbyautumn@users.noreply.github.com>
2020-07-31 20:59:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,18 @@ from discord.ext    import commands
 from discord.ext.commands   import Bot
 import asyncio
  
-bot = commands.Bot(command_prefix='whatever_prefix_u_want_here')
+bot = commands.Bot(command_prefix='%')
  
 @bot.event
 async def on_ready():
-    print ("whatever_text_u_want_here")
+    print("ready")
  
  
 @bot.event
 async def on_message(message):
-    if(message.channel.id == "channel_id_here"):
-        await bot.add_reaction(message, "discord_emote_id_here")
+    if(message.channel.id == "737807052625412208"):
+        await bot.add_reaction(message, "✅")
+        await bot.add_reaction(message, "❌")
  
  
 bot.run("bot_token_here")
